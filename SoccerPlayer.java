709e79dc26cdfa16c17eeffeae7133dae33f26d3
williamshung <williamshung@gmail.com>
2013-11-29 22:54:49 -0600

    getting started

---
+++ SoccerPlayer.java
@@ -0,0 +1,94 @@
+//Curry Williamms
+import java.text.*;
+public class SoccerPlayer
+{
+   DecimalFormat df = new DecimalFormat("#.##");
+   String name=" ";
+   int goals=0;
+   double avegoals=0;
+   int games=0;
+   String team=" ";
+   int playerID=0;
+   static int id=0;
+   public SoccerPlayer()
+   { 
+      name="noname";
+      goals=0;
+      games=0;
+      team="the home";
+      playerID=id;
+      id++;
+   }
+   public SoccerPlayer(String nam,int gam,String tea)
+   {
+      name=nam;
+      games=gam;
+      team=tea;
+      playerID=id;
+      id++;
+   }
+   public String getName()
+   {
+      return name;
+   }
+   public int getGoals()
+   {
+      return goals;
+   }
+   public int getGames()
+   {
+      return games;
+   }   
+   public String getTeam()
+   {
+      return team;
+   }
+   public int getPlayerID()
+   {
+      return playerID;
+   }
+   public double getAveGoals()
+   {
+      return avegoals;
+   }   
+   public void setName(String na)
+   {
+      name=na;
+   }
+   public void setGoals(int go)
+   {
+      goals=go;
+      if((goals<=0)||(goals<=0))
+      avegoals=goals/games;
+      else
+      avegoals=1000000;
+   }
+   public void setGames(int ga)
+   {
+      games=ga;
+   }
+   public void setTeam(String te)
+   {
+      team=te;
+   }
+   public void setAveGoal(double ag)
+   {
+      avegoals=ag;
+   }
+   public String toString()
+   {
+      String output="The players name is "+name+" Goals scored "+goals+" Games played "+games+" The team they play for is "+team+" Their jersey number is "+playerID+" Their id is";
+      return output;
+   }
+   public boolean equals(SoccerPlayer sp)
+   {
+      if((this.getAveGoals()==sp.getAveGoals())&&(this.getTeam()==sp.getTeam()))
+         return true;
+      else
+         return false;
+      
+   }
+   
+}
+
+   
---
+++ SoccerPlayerClient.java
@@ -0,0 +1,71 @@
+//Curry Williams
+import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+public class SoccerPlayerClient
+{
+   public static void main(String[]arg)
+   {  
+      int a=0;
+      String[] stats=new String[6];
+      //stats[1]="check";
+      //System.out.println(stats[1]);
+      int b=0;
+      String lines=" ";
+      try
+		{
+		   File data = new File("players.txt");
+		   Scanner scan1 = new Scanner(data);
+		   while(scan1.hasNext())
+		   {
+            lines=scan1.nextLine();
+            System.out.println(lines);
+		      stats[a]=lines;
+			   a++;
+            //System.out.println(stats[a]);
+
+		   }
+		}
+		catch (IOException ioe)
+		{
+			System.out.println("Error reading file.");
+		}
+      SoccerPlayer[]player=new SoccerPlayer[6];
+      String n;
+ 		int g;
+  		String t; 
+   	for(int c = 0; c < player.length; c++)
+   	   {
+            n = stats[c].split(",")[0];
+            g = Integer.parseInt(stats[c].split(",")[1]);
+   			t = stats[c].split(",")[2];
+   	   	player[c] = new SoccerPlayer(n,g,t);
+               System.out.println(player[c]);
+         }
+         for(int d=0;d<player.length;d++)
+            {
+               System.out.println("Please enter number of goals for player number "+player[d].getPlayerID());
+         	   Scanner scan=new Scanner(System.in);
+		         int goalsinput=scan.nextInt();
+               player[d].setGoals(goalsinput);
+             
+            }
+            SoccerPlayer bestave=player[0];
+            double maxave=0.0;
+            for(int e=0;e<player.length;e++)
+            {
+               if((maxave<player[e].getAveGoals())&&(player[e].getGames()==0));
+               {
+                  maxave=player[e].getAveGoals();
+                  bestave=player[e];
+               }
+               
+            } 
+            System.out.println("The player with the highest average is "+bestave.getName());
+            System.out.println("The Highest average is "+maxave);
+                  
+   }
+   
+   
+}                 
